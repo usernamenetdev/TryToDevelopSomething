5bebde251647025eb5cc0f9c83a8e0c2fa295b56
Павел Живодеров <sosweetpain@live.com>
2023-12-05 11:18:02 +0400

    Push

---
+++ Schemas/UsrCurrencyQuotations1Page/UsrCurrencyQuotations1Page.js
@@ -0,0 +1,100 @@
+define("UsrCurrencyQuotations1Page", [], function() {
+	return {
+		entitySchemaName: "UsrCurrencyQuotations",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrCurrencyQuotationsFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrCurrencyQuotations"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrNamec2106335-a607-4c58-8c0e-3c3364e75349",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0,
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": []
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"name": "Files",
+				"values": {
+					"itemType": BPMSoft.ViewItemType.DETAIL
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"name": "NotesControlGroup",
+				"values": {
+					"itemType": BPMSoft.ViewItemType.CONTROL_GROUP,
+					"caption": {
+						"bindTo": "Resources.Strings.NotesGroupCaption"
+					},
+					"items": []
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"name": "Notes",
+				"values": {
+					"bindTo": "UsrNotes",
+					"dataValueType": BPMSoft.DataValueType.TEXT,
+					"contentType": BPMSoft.ContentType.RICH_TEXT,
+					"layout": {
+						"column": 0,
+						"row": 0,
+						"colSpan": 24
+					},
+					"labelConfig": {
+						"visible": false
+					},
+					"controlConfig": {
+						"imageLoaded": {
+							"bindTo": "insertImagesToNotes"
+						},
+						"images": {
+							"bindTo": "NotesImagesCollection"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
---
+++ Schemas/UsrCurrencyQuotations27b60d8fSection/UsrCurrencyQuotations27b60d8fSection.js
@@ -0,0 +1,8 @@
+define("UsrCurrencyQuotations27b60d8fSection", [], function() {
+	return {
+		entitySchemaName: "UsrCurrencyQuotations",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
